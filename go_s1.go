63222c1c51ea30e286e46d4c0eb6928a47888563
Greenbirch2007 <291109028@qq.com>
2019-12-07 20:47:03 +0800

    go爬虫开始

---
+++ go_s1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+
+
+func download(urlstring) {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	// 自定义Header
+	req.Header.Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("http get error", err)
+		return
+	}
+	//函数结束后关闭相关链接
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read error", err)
+		return
+	}
+	fmt.Println(string(body))
+}
+func main() {
+	fmt.Println("Hello, world")
+	url := "http://www.baidu.com/"
+	download(url)
+}
